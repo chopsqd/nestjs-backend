25462d01a1af1a026eee3c39b6b78cca9c85fda0
Michael Chop <007micle007@gmail.com>
2022-11-20 19:19:26 +0300

    Added authorization methods, using JWT functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import {User} from "./users/users.model";
 import { RolesModule } from './roles/roles.module';
 import {Role} from "./roles/roles.model";
 import {UserRoles} from "./roles/user-roles.model";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
     controllers: [],
@@ -26,6 +27,7 @@ import {UserRoles} from "./roles/user-roles.model";
             autoLoadModels: true
         }),
         RolesModule,
+        AuthModule,
     ]
 })
 export class AppModule {
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -13,6 +13,7 @@ import {RolesModule} from "../roles/roles.module";
   imports: [
       SequelizeModule.forFeature([User, Role, UserRoles]),
       RolesModule
-  ]
+  ],
+    exports: [UsersService]
 })
 export class UsersModule {}
